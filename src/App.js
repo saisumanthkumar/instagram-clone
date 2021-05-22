5e9e687c2a6b43a4ae052f0f946c32322b500e10
saisumanthkumar <saisumanthkumar95509@gmail.com>
2021-05-22 14:03:38 +0530

    Added root pages to different component,like homepage,profile tab.,etc

--- src/App.js
+++ src/App.js
@@ -79,7 +79,7 @@ function App() {
           <Profile />
         </Route>
 
-        <Route path="/instagram-clone/*">
+        <Route path="*">
           <Error />
         </Route>
       </Switch>
